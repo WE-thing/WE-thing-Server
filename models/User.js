ec5a59e219119c86273b45bf33adc884e3930dde
KimRiun <kimriun27@gmail.com>
2024-05-27 14:00:39 +0900

    feat: 회원가입 api 작성

--- models/User.js
+++ models/User.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     id: { type: Number, required: true },
-    userName: { type: String, required: true},
-    role_id: {
+    userName: { type: String, required: true },
+    roleId: {
       type: Number,
       required: true,
       ref: "Role",
     },
-    phoneNumber: { type: String, required: true},
-    relationShipNumber: { type: Number, required: true},
-    relationShipString: { type: String, required: true},
+    phoneNumber: { type: String, required: true },
+    relationShipNumber: { type: Number, required: true },
+    relationShipString: { type: String, required: true },
     attend: Number,
   },
   {
@@ -19,13 +19,13 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.set("toJSON", {virtuals: true});
-userSchema.set("toObject", {virtuals: true});
+userSchema.set("toJSON", { virtuals: true });
+userSchema.set("toObject", { virtuals: true });
 userSchema.virtual("albums", {
   ref: "Album",
   localField: "id",
   foreignField: "userId",
 });
 
-const User = mongoose.model("User", userSchema());
+const User = mongoose.model("User", userSchema);
 module.exports = User;
