b04432d4ae16cc0100bba53b0022d6db54031ec2
KimRiun <kimriun27@gmail.com>
2024-05-27 14:30:31 +0900

    feat: 로그인 api 구현

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,6 +2,10 @@ var express = require("express");
 var router = express.Router();
 const User = require("../../models/User");
 
+/**
+ * POST ~/api/user/signup
+ * 회원가입
+ */
 router.post("/signup", async (req, res, next) => {
   const {
     id,
@@ -29,6 +33,26 @@ router.post("/signup", async (req, res, next) => {
       res.status(400);
       next(err);
     });
+});
+
+router.get("/login", async (req, res, next) => {
+  const { userName, phoneNumber } = req.body;
+
+  try {
+    User.findOne({
+      userName: userName,
+      phoneNumber: phoneNumber,
+    }).then((result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(400).json("존재하지 않는 사용자입니다.");
+      }
+    });
+  } catch (err) {
+    res.status(400);
+    next(err);
+  }
 });
 
 module.exports = router;
