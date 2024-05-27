a93b182755e44baa0e31c49bff1136580bc96330
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-28 03:02:12 +0900

    feat: 이미지 업로드 api(공유 앨범에 추가)

--- middleware/uuid.js
+++ middleware/uuid.js
@@ -1,10 +1,9 @@
-const { v4 } = require('uuid'); // uuid4 사용
+const { v4 } = require("uuid"); // uuid4 사용
 
 // uuid 생성
 const createUUID = () => {
-    const tokens = v4().split('-');
-    console.log("token", tokens); // 잘 생성되었나 확인용
-    return tokens[2] + tokens[1] + tokens[0] + tokens[3] + tokens[4];
+  const tokens = v4().split("-");
+  return tokens[2] + tokens[1] + tokens[0] + tokens[3] + tokens[4];
 };
 
 module.exports = { createUUID };
--- routes/api/album.js
+++ routes/api/album.js
@@ -12,39 +12,40 @@ const setAlbumDirectory = (req, res, next) => {
 router.get("/:id", async (req, res) => {
   const pics = await Album.find({ id: req.params.id });
   res.send(pics);
-  console.log(pics);
 });
 
 const addFiles = async (req, res) => {
   let pictureUrls = [];
-  console.log(req.headers);
   if (req.files && req.files.length > 0) {
     pictureUrls = req.files.map((file) => file.location); // S3 URL 추출
-    console.log(pictureUrls);
-
-    console.log("picture: ", pictureUrls);
-    const exist = await Album.findOne({ userId: req.params.userId });
-    if (exist) {
-      const existingUrls = JSON.parse(exist.imageUrl);
-      const combinedUrls = existingUrls.concat(pictureUrls);
-      exist.imageUrl = JSON.stringify(combinedUrls);
-      await exist.save();
-    } else {
-      const cnt = await Album.countDocuments();
-      await Album.create({
-        id: cnt + 1,
-        imageUrl: JSON.stringify(pictureUrls),
-        userId: req.params.userId,
-      });
-    }
-    console.log("finish Logic");
-    res.send("end");
   } else {
     // 사진이 첨부되지 않았을 경우의 처리
-    return res.send("no image");
+    return res.send("IMAGE_NOT_EXIST");
   }
+  const exist = await Album.findOne({ userId: req.params.userId });
+  if (exist) {
+    const existingUrls = JSON.parse(exist.imageUrl);
+    const combinedUrls = existingUrls.concat(pictureUrls);
+    await Album.updateOne(
+      { userId: req.params.userId },
+      { imageUrl: JSON.stringify(combinedUrls) }
+    );
+  } else {
+    const cnt = await Album.countDocuments();
+    await Album.create({
+      id: cnt + 1,
+      imageUrl: JSON.stringify(pictureUrls),
+      userId: req.params.userId,
+    });
+  }
+  res.send("The End");
 };
 
-router.post("/:userId", setAlbumDirectory, imageUploader.array("image"), addFiles);
+router.post(
+  "/:userId",
+  setAlbumDirectory,
+  imageUploader.array("image"),
+  addFiles
+);
 
 module.exports = router;
