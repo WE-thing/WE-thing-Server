4f3ac03325dd6fa6394859f4fd101979f093b897
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-27 14:15:15 +0900

    (WIP) 앨범 사진 불러오기 초안 작성

--- routes/api/album.js
+++ routes/api/album.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const Board = require('../../models/Album');
+const Album = require('../../models/Album');
+
+router.get('/:id', async (req, res) => {
+    const pics = await Album.find({id:req.params.id});
+    if (pics.length === 0) {
+        return res.status(404).json({ message: 'Albums not found' });
+    }
+    res.json(pics);
+    console.log('pics');
+});
+
+module.exports = router;
