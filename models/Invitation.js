1df73f76bdc14987564be1e20011ae3c9b10bc3a
donginLee <donginyee@gmail.com>
2024-05-28 11:09:11 +0900

    feat: expreDateTime 미들웨어 사용해서 자동 설정 추가 및 삭제 여부 삭제 날짜 구현

--- models/Invitation.js
+++ models/Invitation.js
@@ -12,8 +12,12 @@ const invitationSchema = new mongoose.Schema(
       default: Date.now,
     },
     weddingDateTime: {
-      type: Date,
+      type: String,
       required: true,
+    },
+    valid: {
+      type: Boolean,
+      default: true,
     },
     locationName: {
       type: String,
@@ -25,19 +29,27 @@ const invitationSchema = new mongoose.Schema(
     },
     locationContact: {
       type: String,
-      required: true,
     },
     galleryImageUrl: {
-      type: String, // 필수 항목 아님
+      type: Array, // 필수 항목 아님
     },
     themeId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Theme",
+      type: Number,
       required: true,
     },
     mainPhoto: {
       type: String,
       required: true,
+    },
+    expireDateTime: {
+      type: String,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedDate: {
+      type: Date,
     },
   },
   {
@@ -55,6 +67,25 @@ invitationSchema.virtual("couples", {
   ref: "Couple",
   localField: "id",
   foreignField: "invitationId",
+});
+
+// weddingDateTime에 기반하여 expireDateTime을 설정하는 pre-save 미들웨어
+invitationSchema.pre("save", function (next) {
+  if (this.isNew) {
+    const weddingDateStr = this.weddingDateTime;
+    const year = parseInt(weddingDateStr.substring(0, 4), 10);
+    const month = parseInt(weddingDateStr.substring(4, 6), 10) - 1; // JavaScript의 월은 0부터 시작
+    const day = parseInt(weddingDateStr.substring(6, 8), 10);
+    const hour = parseInt(weddingDateStr.substring(8, 10), 10);
+
+    const weddingDate = new Date(Date.UTC(year, month, day, hour));
+    const expireDate = new Date(weddingDate);
+    expireDate.setDate(weddingDate.getUTCDate() + 7); // 결혼식 후 7일 추가
+    expireDate.setUTCHours(23, 59, 0, 0); // 23:59로 설정
+
+    this.expireDateTime = expireDate.toISOString();
+  }
+  next();
 });
 
 const Invitation = mongoose.model("Invitation", invitationSchema);
