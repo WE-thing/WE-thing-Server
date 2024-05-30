eb89385a85eeff6fd27a55c966b59b7f2e984994
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-30 14:15:10 +0900

    feat: 손님 내정보 데이터 반환 API 추가 및 user.js authenticate 함수 수정

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
+const userRouter = require("./user").router;
+
 router.use("/album", require("./album"));
-router.use("/user", require("./user"));
+router.use("/user", userRouter);
 router.use("/invitations", require("./invitations"));
 router.use("/message", require("./message"));
 router.use("/couple", require("./couple"));
+router.use("/info", require("./info"));
 
 module.exports = router;
